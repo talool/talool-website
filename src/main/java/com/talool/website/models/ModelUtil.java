825bba9ce210fa3f624c5bc2574a28784f73cf33
Chris Lintz <christopher.justin@gmail.com>
2013-04-07 22:42:20 -0600

    checkpoint

---
+++ src/main/java/com/talool/website/models/ModelUtil.java
@@ -0,0 +1,11 @@
+package com.talool.website.models;
+
+/**
+ * 
+ * @author clintz
+ * 
+ */
+public final class ModelUtil
+{
+
+}
--- src/main/java/com/talool/website/pages/BasePage.java
+++ src/main/java/com/talool/website/pages/BasePage.java
@@ -4,6 +4,9 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import com.talool.core.service.TaloolService;
+import com.talool.service.ServiceFactory;
+
 /**
  * 
  * @author clintz
@@ -12,6 +15,9 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 public abstract class BasePage extends WebPage
 {
 	private static final long serialVersionUID = -7463278066879672957L;
+
+	protected transient static final TaloolService taloolService = ServiceFactory.get()
+			.getTaloolService();
 
 	public BasePage()
 	{
