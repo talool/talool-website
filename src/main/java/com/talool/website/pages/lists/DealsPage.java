918518b8d7749eb63fe7c55742f7a57019d92867
douglasmccuen <douglasmccuen@gmail.com>
2013-04-08 22:09:30 -0600

    adding repeaters for deals and friends

--- src/main/java/com/talool/website/pages/lists/DealsPage.java
+++ src/main/java/com/talool/website/pages/lists/DealsPage.java
@@ -1,7 +1,14 @@
 package com.talool.website.pages.lists;
 
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import com.talool.core.Deal;
+import com.talool.website.models.DealListModel;
 import com.talool.website.pages.BasePage;
 import com.talool.website.panel.AdminMenuPanel;
 
@@ -25,6 +32,40 @@ public class DealsPage extends BasePage {
 		super.onInitialize();
 
 		add(new AdminMenuPanel("adminMenuPanel").setRenderBodyOnly(true));
+		
+		DealListModel model = new DealListModel();
+		model.setMerchantId(1);
+		
+		final ListView<Deal> deals = new ListView<Deal>("dealRptr", model)
+		{
+
+			private static final long serialVersionUID = -7055359730593625591L;
+
+			@Override
+			protected void populateItem(ListItem<Deal> item)
+			{
+				Deal deal = item.getModelObject();
+
+				item.setModel(new CompoundPropertyModel<Deal>(deal));
+
+				if (item.getIndex() % 2 == 0)
+				{
+					item.add(new AttributeModifier("class", "odd"));
+				}
+				else
+				{
+					item.add(new AttributeModifier("class", "even"));
+				}
+
+				item.add(new Label("title"));
+				item.add(new Label("summary"));
+				item.add(new Label("isActive"));
+				item.add(new Label("expires"));
+			}
+
+		};
+
+		add(deals);
 	}
 
 }
