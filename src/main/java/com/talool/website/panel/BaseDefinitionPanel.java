3493ed356f17efa74b0a02d6c7d387ef6a26010e
douglasmccuen <douglasmccuen@gmail.com>
2013-04-22 09:22:11 -0600

    adding default return submit action for "save" button all modals.

--- src/main/java/com/talool/website/panel/BaseDefinitionPanel.java
+++ src/main/java/com/talool/website/panel/BaseDefinitionPanel.java
@@ -2,7 +2,7 @@ package com.talool.website.panel;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.CompoundPropertyModel;
@@ -43,7 +43,7 @@ abstract public class BaseDefinitionPanel extends BasePanel
 
 		form.setDefaultModel(getDefaultCompoundPropertyModel());
 
-		AjaxSubmitLink submit = new AjaxSubmitLink("submitButton", form)
+		AjaxButton submit = new AjaxButton("submitButton", form)
 		{
 
 			private static final long serialVersionUID = -6562989540935949813L;
@@ -84,7 +84,6 @@ abstract public class BaseDefinitionPanel extends BasePanel
 			}
 
 		};
-
 		form.add(submit);
 		submit.add(new Label("submitLabel", getSaveButtonLabel()));
 		
