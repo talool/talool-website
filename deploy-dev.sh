c28335d053481362a1247cefb431f901e6f3fc2e
Chris Lintz <christopher.justin@gmail.com>
2013-06-13 01:00:57 -0600

    dev deploy script changes

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,6 +2,7 @@
 
 set -e
 server=dev-app1
+buildDir=/opt/talool/builds
 
 if [ -n "$1" ]; then
   warFile="$1"
@@ -9,11 +10,13 @@ else
  warFile=$(ls target/*SNAPSHOT.war)
 fi
 
-warFileName=$(basename $warFile) 
+warFileName=$(basename $warFile)
 
 read -s -p "sudo pass on $server? " pass
 
+ssh -t $server "echo $pass | sudo -S rm -rf $buildDir/$warFileName"
+
 echo "\nDeploying $warFile..."
-#scp "$warFile" $server:/opt/talool/builds
+scp -p "$warFile" $server:$buildDir
 
 ssh -t $server "echo $pass | sudo -S /opt/talool/builds/deploy.sh $warFileName"
