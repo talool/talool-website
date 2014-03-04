bbd6b5ab7bb651d2a2f4e1b6b0e70766c1d7f249
Chris Lintz <christopher.justin@gmail.com>
2014-03-03 22:03:45 -0700

    Mobile pages for gift return

---
+++ src/main/java/com/talool/website/mobile/MobileGiftReturnedPage.java
@@ -0,0 +1,32 @@
+package com.talool.website.mobile;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import com.talool.website.pages.corporate.BaseCorporatePage;
+
+/**
+ * This page describes to the user the reasons why a gift was returned
+ * 
+ * @author clintz
+ * 
+ */
+public class MobileGiftReturnedPage extends BaseCorporatePage
+{
+
+	public MobileGiftReturnedPage(PageParameters params)
+	{
+		super(params);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void onInitialize()
+	{
+		super.onInitialize();
+	}
+
+}
