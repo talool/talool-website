249ce0900faf686b7f67e9e009622fd407af2647
Doug McCuen <doug@talool.com>
2014-03-10 17:28:47 -0600

    removed the autocomplete from merchant search

--- src/main/java/com/talool/website/component/MerchantSearchPanel.java
+++ src/main/java/com/talool/website/component/MerchantSearchPanel.java
@@ -2,25 +2,20 @@ package com.talool.website.component;
 
 import java.util.UUID;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
-import org.wicketstuff.objectautocomplete.ObjectAutoCompleteField;
-import org.wicketstuff.objectautocomplete.ObjectAutoCompleteSelectionChangeListener;
-
-import com.talool.core.Merchant;
-import com.talool.website.util.SessionUtils;
 
 public abstract class MerchantSearchPanel extends Panel {
 
 	private static final long serialVersionUID = 1L;
 	private String merchantName;
 	private UUID merchantId;
-	private boolean acSelected = false;
-	private ObjectAutoCompleteField<Merchant, UUID> acField;
+	private TextField<String> nameField;
 
 	public MerchantSearchPanel(String id) {
 		super(id);
@@ -34,61 +29,30 @@ public abstract class MerchantSearchPanel extends Panel {
 		Form form = new Form("form");
 		add(form);
 		
+		nameField = new TextField<String>("searchInput", new PropertyModel(this,"merchantName"));
+		form.add(nameField.setOutputMarkupId(true));
+		
 		form.add(new AjaxButton("btn")
 		{
 			private static final long serialVersionUID = 7111919213900349555L;
 
 			@Override
-			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) 
+			{
 				
-				if (acSelected)
+				if (StringUtils.isEmpty(merchantName))
 				{
-					onSearch(target, merchantName);
-					acSelected = false;
-					
-					// WATCH OUT: We are putting the selected name back in the field, 
-					// bc we cleared it on selection to avoid the "selected" state.
-					acField.getSearchTextField().getModel().setObject(merchantName);
+					onSearch(target, "");
 				}
 				else
 				{
-					// only search if the merchant name has changed
-					String val = acField.getSearchTextField().getValue();
-					if (!val.equals(merchantName))
-					{
-						merchantName = val;
-						onSearch(target, "*"+merchantName+"*");
-					}
+					onSearch(target, "*"+merchantName+"*");
 				}
 				
 			}
 			
 		});
 
-		
-		// Auto-complete search field for merchant selection
-		MerchantAutoCompleteFactory acFactory = new MerchantAutoCompleteFactory(SessionUtils.getSession().getMerchantAccount().getMerchant());
-		ObjectAutoCompleteSelectionChangeListener<UUID> listener = new ObjectAutoCompleteSelectionChangeListener<UUID>()
-	    {
-
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void selectionChanged(AjaxRequestTarget target, IModel<UUID> model) {
-				merchantName = acField.getSearchTextField().getValue();
-				acSelected = true;
-			}
-	    	
-	    };
-	    acFactory.builder().updateOnSelectionChange(listener);
-	    acFactory.builder().readOnlyRenderer(null);
-	    
-	    // WATCH OUT: We are using this setting because 
-	    // we don't want to see the "selected" state of the component.
-	    acFactory.builder().clearInputOnSelection(); 
-	    
-		acField = acFactory.builder().build("ac", new PropertyModel<UUID>(this, "merchantId"));
-	    form.add(acField);
 	   
 	}
 	
