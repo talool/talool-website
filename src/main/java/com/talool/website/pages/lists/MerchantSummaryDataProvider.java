25bc49f081df75d6601b078f14d78dcfc0aeee36
Doug McCuen <doug@talool.com>
2014-03-21 13:55:26 -0600

    fixed the "and" clause for merchants

--- src/main/java/com/talool/website/pages/lists/MerchantSummaryDataProvider.java
+++ src/main/java/com/talool/website/pages/lists/MerchantSummaryDataProvider.java
@@ -211,8 +211,7 @@ public class MerchantSummaryDataProvider implements IDataProvider<MerchantSummar
 		}
 		else
 		{
-			criteria.setFilters(Filter.keyDoesNotExistOrPropertiesNull(KeyValue.fundraiser));
-			criteria.setFilters(Filter.and(Filter.keyDoesNotExistOrPropertiesNull(KeyValue.publisher)));
+			criteria.setFilters(Filter.and(Filter.keyDoesNotExistOrPropertiesNull(KeyValue.publisher),Filter.keyDoesNotExistOrPropertiesNull(KeyValue.fundraiser)));
 		}
 		return criteria;
 	}
