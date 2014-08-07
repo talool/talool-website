42a353451927bc7b35a475e2d4360e559f116a49
Doug McCuen <doug@talool.com>
2014-08-07 14:36:24 -0600

    clean up the confirmation message

--- src/main/java/com/talool/website/panel/message/wizard/MessageConfirmationStep.java
+++ src/main/java/com/talool/website/panel/message/wizard/MessageConfirmationStep.java
@@ -1,5 +1,8 @@
 package com.talool.website.panel.message.wizard;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.apache.wicket.extensions.wizard.WizardStep;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
@@ -8,6 +11,7 @@ import org.apache.wicket.model.ResourceModel;
 import com.talool.core.FactoryManager;
 import com.talool.core.service.CustomerService;
 import com.talool.core.service.ServiceException;
+import com.talool.website.models.DealOfferModel;
 import com.talool.website.panel.deal.DealPreview;
 import com.talool.website.panel.message.MerchantGift;
 
@@ -32,9 +36,14 @@ public class MessageConfirmationStep extends WizardStep
 		
 		WebMarkupContainer descriptionPanel = new WebMarkupContainer("descriptionPanel");
 		addOrReplace(descriptionPanel.setOutputMarkupId(true));
+		String gender = (mg.getCriteria().getSex() == null)?"Any":mg.getCriteria().getSex().name();
 
-		descriptionPanel.add(new Label("title",mg.getDeal().getTitle()));
+		DealOfferModel dom = new DealOfferModel(mg.getCriteria().getDealOfferId());
 		descriptionPanel.add(new Label("count",getCustomerCount()));
+		descriptionPanel.add(new Label("book",dom.getObject().getTitle()));
+		descriptionPanel.add(new Label("gender",gender));
+		descriptionPanel.add(new Label("minAge",getAgeForDate(mg.getCriteria().getOlderThan())));
+		descriptionPanel.add(new Label("maxAge",getAgeForDate(mg.getCriteria().getYoungerThan())));
 		
 		// display a deal/gift preview and update it on selection
 		final DealPreview dealPreview = new DealPreview("dealBuilder", mg.getDeal());
@@ -57,6 +66,24 @@ public class MessageConfirmationStep extends WizardStep
 			customerCount = 0l;
 		}
 		return customerCount;
+	}
+	
+	private String getAgeForDate(Date date)
+	{
+		if (date==null)
+		{
+			return "Any";
+		}
+		else
+		{
+			Calendar c = Calendar.getInstance();
+			int currentYear = c.get(Calendar.YEAR);
+			c.setTime(date);
+			int birthYear = c.get(Calendar.YEAR);
+			Integer age = currentYear - birthYear;
+			return age.toString();
+		}
+		
 	}
 
 }
