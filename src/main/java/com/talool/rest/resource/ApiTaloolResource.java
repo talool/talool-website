25689ec098cb209ac3ae5f7e82a01e6394fae38a
Doug McCuen <doug@talool.com>
2014-09-30 09:21:26 -0600

    Set an open CORs header for the rest api

--- src/main/java/com/talool/rest/resource/ApiTaloolResource.java
+++ src/main/java/com/talool/rest/resource/ApiTaloolResource.java
@@ -1,8 +1,10 @@
 package com.talool.rest.resource;
 
+import org.apache.wicket.request.http.WebResponse;
 import org.wicketstuff.rest.contenthandling.json.objserialdeserial.GsonObjectSerialDeserial;
 import org.wicketstuff.rest.contenthandling.json.webserialdeserial.JsonWebSerialDeserial;
 import org.wicketstuff.rest.resource.AbstractRestResource;
+import org.wicketstuff.rest.resource.MethodMappingInfo;
 
 import com.talool.core.FactoryManager;
 import com.talool.core.service.TaloolService;
@@ -18,5 +20,15 @@ public class ApiTaloolResource extends AbstractRestResource<JsonWebSerialDeseria
 		super(new JsonWebSerialDeserial(new GsonObjectSerialDeserial()));
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	protected void onAfterMethodInvoked(MethodMappingInfo mappedMethod,
+			Attributes attributes, Object result) {
+		super.onAfterMethodInvoked(mappedMethod, attributes, result);
+		
+		((WebResponse) getCurrentWebResponse()).setHeader("Access-Control-Allow-Origin", "*" );
+	}
+	
+	
 
 }
